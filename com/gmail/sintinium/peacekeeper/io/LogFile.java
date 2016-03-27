4a8c92f3487e41d3babc85fcc421ba7e4ad7a537
MarcusJBell <marcusjustinbell@gmail.com>
2016-03-27 04:40:37 -0500

    Added filter for books, signs, and item renaming

--- com/gmail/sintinium/peacekeeper/io/LogFile.java
+++ com/gmail/sintinium/peacekeeper/io/LogFile.java
@@ -62,4 +62,50 @@ public class LogFile {
         }
     }
 
+    public void logBook(String player, String message) {
+        File folder = new File(peacekeeper.getDataFolder(), File.separator + "logs");
+        if (!folder.exists())
+            folder.mkdir();
+        File file = new File(peacekeeper.getDataFolder(), File.separator + "logs" + File.separator + "booklogs.txt");
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            FileWriter fileWriter = new FileWriter(file, true);
+            PrintWriter writer = new PrintWriter(fileWriter);
+            writer.println("[" + TimeUtils.formatTime(System.currentTimeMillis()) + "] (" + player + ") " + message);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void logSign(String player, String message) {
+        File folder = new File(peacekeeper.getDataFolder(), File.separator + "logs");
+        if (!folder.exists())
+            folder.mkdir();
+        File file = new File(peacekeeper.getDataFolder(), File.separator + "logs" + File.separator + "signlogs.txt");
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            FileWriter fileWriter = new FileWriter(file, true);
+            PrintWriter writer = new PrintWriter(fileWriter);
+            writer.println("[" + TimeUtils.formatTime(System.currentTimeMillis()) + "] (" + player + ") " + message);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- com/gmail/sintinium/peacekeeper/listeners/ChatFilter.java
+++ com/gmail/sintinium/peacekeeper/listeners/ChatFilter.java
@@ -11,12 +11,17 @@ import com.gmail.sintinium.peacekeeper.utils.CommandUtils;
 import org.apache.commons.lang.ArrayUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerEditBookEvent;
 
 import java.util.HashSet;
 import java.util.Map;
@@ -35,10 +40,9 @@ public class ChatFilter implements Listener {
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onChatMessage(AsyncPlayerChatEvent event) {
-        long time = System.currentTimeMillis();
         if (checkFilter(event.getPlayer(), event.getMessage())) {
             event.setCancelled(true);
-            broadcastFilter(event.getPlayer(), event.getMessage());
+            broadcastFilter(event.getPlayer(), event.getMessage(), 0);
         } else {
             for (Map.Entry<String, String> e : peacekeeper.chatFilter.replacedWords.entrySet()) {
                 if (event.getMessage().contains(e.getKey())) {
@@ -46,7 +50,6 @@ public class ChatFilter implements Listener {
                 }
             }
         }
-        Bukkit.getConsoleSender().sendMessage(System.currentTimeMillis() - time + "");
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
@@ -57,12 +60,58 @@ public class ChatFilter implements Listener {
         if (m.equalsIgnoreCase("/r") || m.equalsIgnoreCase("/msg") || m.equalsIgnoreCase("/tell") || m.equalsIgnoreCase("/me") || m.equalsIgnoreCase("/say") || m.equalsIgnoreCase("/afk") || m.equalsIgnoreCase("/m") || m.equalsIgnoreCase("/whisper")) {
             if (checkFilter(event.getPlayer(), event.getMessage())) {
                 event.setCancelled(true);
-                broadcastFilter(event.getPlayer(), event.getMessage());
+                broadcastFilter(event.getPlayer(), event.getMessage(), 1);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onBookEditEvent(PlayerEditBookEvent event) {
+        String book;
+        StringBuilder builder = new StringBuilder();
+        for (String s : event.getNewBookMeta().getPages()) {
+            builder.append(s).append(" ");
+        }
+        book = builder.toString();
+        Peacekeeper.logFile.logBook(event.getPlayer().getName(), book);
+        if (checkFilter(event.getPlayer(), book)) {
+            event.setCancelled(true);
+            broadcastFilter(event.getPlayer(), book, 2);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onSignEdit(SignChangeEvent event) {
+        String sign;
+        StringBuilder builder = new StringBuilder();
+        for (String s : event.getLines()) {
+            builder.append(s).append(" ");
+        }
+        sign = builder.toString();
+        Peacekeeper.logFile.logSign(event.getPlayer().getName(), sign);
+        if (checkFilter(event.getPlayer(), sign)) {
+            event.setCancelled(true);
+            broadcastFilter(event.getPlayer(), sign, 3);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onRename(InventoryClickEvent event) {
+        if (event.getClickedInventory() != null && event.getClickedInventory().getType() == InventoryType.ANVIL) {
+            if (event.getRawSlot() == 2) {
+                Player p = Bukkit.getPlayer(event.getWhoClicked().getUniqueId());
+                if (p == null || event.getCurrentItem() == null || event.getCurrentItem().getType() == Material.AIR || event.getCurrentItem().getItemMeta() == null || event.getCurrentItem().getItemMeta().getDisplayName() == null)
+                    return;
+                if (checkFilter(p, event.getCurrentItem().getItemMeta().getDisplayName())) {
+                    event.setCancelled(true);
+                    broadcastFilter(p, event.getCurrentItem().getItemMeta().getDisplayName(), 4);
+                }
             }
         }
     }
 
     private boolean checkFilter(Player player, String message) {
+        message = message.toLowerCase();
 //        final String originalMessage = message;
         if (CommandUtils.containsNumber(message, 6) || message.contains("server") || message.contains("craft") || message.contains(".ws") || message.contains(".no")) {
             if (checkStrictIP(player, message)) {
@@ -82,7 +131,6 @@ public class ChatFilter implements Listener {
         if (last == '.' || last == ',' || last == '!' || last == '?') {
             clipped = clipped.substring(0, clipped.length() - 1);
         }
-        Bukkit.getConsoleSender().sendMessage(clipped);
 
         Pattern pattern = Pattern.compile(CommandUtils.CONTAINS_SPECIAL_CHAR);
         String wildcarded = pattern.matcher(ChatColor.stripColor(clipped)).replaceAll("[A-Za-z0-9]");
@@ -158,10 +206,6 @@ public class ChatFilter implements Listener {
         for (Map.Entry<Character[], Long> e : peacekeeper.chatFilter.strictChars.entrySet()) {
             char[] c = removeChar(ArrayHelper.covertCharArray(e.getKey()), '.');
 
-            for (char ee : c) {
-                Bukkit.getConsoleSender().sendMessage(ee + "");
-            }
-
             int index = 0;
             for (char sc : message.toCharArray()) {
                 if (sc == c[index]) {
@@ -178,7 +222,6 @@ public class ChatFilter implements Listener {
     }
 
     private void handleBan(final Player player, final long length) {
-        Bukkit.getConsoleSender().sendMessage(length + "");
         peacekeeper.databaseQueueManager.scheduleTask(new IQueueableTask() {
             @Override
             public void runTask() {
@@ -199,10 +242,31 @@ public class ChatFilter implements Listener {
         });
     }
 
-    private void broadcastFilter(Player player, String message) {
+    // 0 = chat, 1 = command, 2 = book, 3 = sign, 4 = item
+    private void broadcastFilter(Player player, String message, int type) {
         String m = ChatColor.DARK_RED + player.getName() + ChatColor.RED + " used blocked word(s): " + message;
+        String typeMessage;
+        switch (type) {
+            case 0:
+                typeMessage = "Chat";
+                break;
+            case 1:
+                typeMessage = "Command";
+                break;
+            case 2:
+                typeMessage = "Book";
+                break;
+            case 3:
+                typeMessage = "Sign";
+                break;
+            case 4:
+                typeMessage = "Item";
+                break;
+            default:
+                typeMessage = "ERROR";
+        }
+        m += "\n" + ChatColor.DARK_AQUA + "Type: " + ChatColor.AQUA + typeMessage;
         player.sendMessage(m);
-        Bukkit.getConsoleSender().sendMessage(m);
         for (Player p : Bukkit.getOnlinePlayers()) {
             if (p.equals(player)) continue;
             if (p.hasPermission("peacekeeper.command.mute")) {
